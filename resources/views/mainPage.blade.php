23f8f1ae32568a83ea5e54c1f073ec2bda071672
Wazeback <david.lelystad@gmail.com>
2022-03-08 09:53:40 +0100

    'update'

---
+++ resources/views/mainPage.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About me</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+</head>
+<body class="w-[100vw] h-[100vh] justify-center items-center flex bg-indigo-500">
+    <div class="flex w-[92%] h-[92%] bg-white rounded-[4rem]">
+        <div class="w-1/6 flex justify-center items-center flex-col pl-6">
+            <button class="text-2xl bg-blue-500 hover:bg-blue-400 text-white font-bold text-lg py-4 px-8 border-b-8 border-blue-700 hover:border-blue-500 rounded w-46 h-24">
+                Contact me
+            </button>
+            <div class="pt-24">
+                Lorem ipsum dolor, sit amet consec veritatis! At magni architecto perspiciatis? Itaque odio sed quibusdam quia est quae, sit quaerat assumenda. Aut adipisci dolor, delectus quod quae id animi ipsum, vel doloribus dolores saepe facilis doloremque nesciunt quo consequatur facere sapiente voluptatibus veniam. Expedita ab exercitationem ad sed omnis fugit ratione repudiandae soluta dolorum, pariatur officia dolores, eaque sint quis quo doloremque rerum inventore quaerat tenetur recusandae odit. Ea, soluta laborum!
+            </div> 
+        </div>
+
+
+        <div class="border-2 w-0 h-[85%] m-12">
+
+        </div>
+
+        <div class="w-2/6 flex justify-center flex-col">
+            <div>            
+                <h1 class="font-bold leading-tight text-8xl mt-0 mb-2 text-blue-600">David Vos</h1>
+                <h1 class="font-exralight leading-tight text-2xl mt-0 mb-2 text-blue-600">/Wazeback</h1>
+            </div>
+            <div class="flex w-full pt-6">
+                <div class="p-2">
+                    Mijn specialitieten zijn HTML, CSS en PHP. Ook kan ken ik de basis van python. Hiernaast heb ik meerdere kleine projecten gemaakt in programeertalen zoals P5.js, java, c++ & c.
+                </div>
+                <div class="p-2"> 
+                    Een groot deel van mijn projecten staan op deze website. als je de code door wil lezen en meer informatie wil hebben over de projecten zelf kan je altijd mijn mijn <a href="https://github.com/Wazeback">github.</a>
+                </div>
+            </div>
+        </div>
+        <div class="">
+            <img src="magicpattern-blob-1646724009546.png"> 
+        </div>
+    <div>
+</body>
+</html>
